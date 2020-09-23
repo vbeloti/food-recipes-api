b4c06fbcd1af5fb1a2ff55591c7c3f17897143f3
unknown <viniciusbeloti7@gmail.com>
2020-09-23 12:01:26 -0300

    chore: add typeorm and your configurations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import 'reflect-metadata'
 import express from 'express'
+
+import './database/connect'
 import routes from './routes'
 
 const app = express()
@@ -6,4 +9,4 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => console.log('🔥 Server started...'))
+app.listen(3333, () => console.log('🔥 Server started...'))
